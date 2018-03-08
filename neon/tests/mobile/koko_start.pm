2beecb0d077034fec55adc2872c1921648ee36b0
Bhavisha Dhruve <bhavishadhruve@gmail.com>
2018-03-08 15:46:06 +0530

    Add test cases for Koko gallery
    - Opens the koko gallery and verifies it is started.

---
+++ neon/tests/mobile/koko_start.pm
@@ -0,0 +1,49 @@
+
+# Copyright (C) 2018 Bhavisha Dhruve <bhavishadhruve@gmail.com>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License or (at your option) version 3 or any later version
+# accepted by the membership of KDE e.V. (or its successor approved
+# by the membership of KDE e.V.), which shall act as a proxy
+# defined in Section 14 of version 3 of the license.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use base "basetest";
+use strict;
+use testapi;
+
+sub run {
+    wait_still_screen;
+    send_key 'ret';
+
+    # wait for plasma-mobile screen to appear.
+    assert_screen 'plasma-mobile-start', 60;
+    assert_and_click 'plasma-mobile-secsearch';
+    type_string 'gallery';
+    send_key 'tab';
+    assert_and_click 'plasma-mobile-selectkoko';
+    assert_screen 'plasma-mobile-kokoscreen', 40;
+    assert_and_click 'plasma-mobile-closekoko';
+
+}
+
+sub test_flags {
+    # without anything - rollback to 'lastgood' snapshot if failed
+    # 'fatal' - whole test suite is in danger if this fails
+    # 'milestone' - after this test succeeds, update 'lastgood'
+    # 'important' - if this fails, set the overall state to 'fail'
+    return { important => 1 };
+}
+
+1;
+
+
